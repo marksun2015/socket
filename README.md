de1a487d07f4dca7eda741d339ff613647562900
MarkSun <mark@weintek.com>
2017-08-08 14:07:56 +0800

    New code for package filter

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-+ ref:  
-http://beej-zhtw.netdpi.net/ 
-
 + 1.clientservertcp  
- $ server portnumber  
- $ clinet 127.0.0.1 portnumber  
+    $ server portnumber  
+    $ clinet 127.0.0.1 portnumber  
 
 + 2.clientserverudp  
- $ server   
- $./client 127.0.0.1 4950  
+    $ server   
+    $./client 127.0.0.1 4950  
 
 + 3.select   
- $ telnet 192.168.1.181 9034  
+    $ telnet 192.168.1.181 9034  
+
++ 4.filter
+    //package filter
+    $ gcc -o filter filter.c
+    $ sudo ./filter
+
++ ref  
+    socket:
+    http://beej-zhtw.netdpi.net/
+
+    filter:
+    http://www.cnblogs.com/jinrize/archive/2009/11/24/1609902.html
---
+++ filter/filter.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/filter.h>
+#include <net/if.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <netinet/ether.h>
+#include <net/route.h>
+
+
+int main(int argc, char **argv) {
+  int sock, n;
+  char buffer[2048];
+  unsigned char *iphead, *ethhead;
+  struct ifreq ethreq;
+
+  /*
+    tcpdump -dd host 192.168.1.179
+  */
+  struct sock_filter BPF_code[]= {
+	{ 0x28, 0, 0, 0x0000000c },
+	{ 0x15, 0, 4, 0x00000800 },
+	{ 0x20, 0, 0, 0x0000001a },
+	{ 0x15, 8, 0, 0xc0a801b3 },
+	{ 0x20, 0, 0, 0x0000001e },
+	{ 0x15, 6, 7, 0xc0a801b3 },
+	{ 0x15, 1, 0, 0x00000806 },
+	{ 0x15, 0, 5, 0x00008035 },
+	{ 0x20, 0, 0, 0x0000001c },
+	{ 0x15, 2, 0, 0xc0a801b3 },
+	{ 0x20, 0, 0, 0x00000026 },
+	{ 0x15, 0, 1, 0xc0a801b3 },
+	{ 0x6, 0, 0, 0x00040000 },
+	{ 0x6, 0, 0, 0x00000000 },
+  };                            
+  struct sock_fprog Filter;
+    
+  Filter.len = 14;
+  Filter.filter = BPF_code;
+  
+  if ( (sock=socket(PF_PACKET, SOCK_RAW,
+                    htons(ETH_P_IP)))<0) {
+    perror("socket");
+    //exit(1);
+  }
+
+  /* Set the network card in promiscuos mode */
+  //strncpy(ethreq.ifr_name,"enx00e04c534458",IFNAMSIZ);
+  strncpy(ethreq.ifr_name,"eth0",IFNAMSIZ);
+  if (ioctl(sock,SIOCGIFFLAGS,&ethreq)==-1) {
+    perror("ioctl");
+    close(sock);
+    //exit(1);
+  }
+  ethreq.ifr_flags|=IFF_PROMISC;
+  if (ioctl(sock,SIOCSIFFLAGS,&ethreq)==-1) {
+    perror("ioctl");
+    close(sock);
+    //exit(1);
+  }
+
+  /* Attach the filter to the socket */
+  if(setsockopt(sock, SOL_SOCKET, SO_ATTACH_FILTER,
+                &Filter, sizeof(Filter))<0){
+    perror("setsockopt");
+    close(sock);
+    //exit(1);
+  }
+
+  while (1) {
+    printf("----------\n");
+    n = recvfrom(sock,buffer,2048,0,NULL,NULL);
+    printf("%d bytes read\n",n);
+
+    /* Check to see if the packet contains at least
+     * complete Ethernet (14), IP (20) and TCP/UDP
+     * (8) headers.
+     */
+    if (n<42) {
+      perror("recvfrom():");
+      printf("Incomplete packet (errno is %d)\n",
+             errno);
+      close(sock);
+      //exit(0);
+    }
+
+    ethhead = buffer;
+    printf("Source MAC address: "
+           "%02x:%02x:%02x:%02x:%02x:%02x\n",
+           ethhead[0],ethhead[1],ethhead[2],
+           ethhead[3],ethhead[4],ethhead[5]);
+    printf("Destination MAC address: "
+           "%02x:%02x:%02x:%02x:%02x:%02x\n",
+           ethhead[6],ethhead[7],ethhead[8],
+           ethhead[9],ethhead[10],ethhead[11]);
+
+    iphead = buffer+14; /* Skip Ethernet  header */
+    if (*iphead==0x45) { /* Double check for IPv4
+                          * and no options present */
+      printf("Source host %d.%d.%d.%d\n",
+             iphead[12],iphead[13],
+             iphead[14],iphead[15]);
+      printf("Dest host %d.%d.%d.%d\n",
+             iphead[16],iphead[17],
+             iphead[18],iphead[19]);
+      printf("Source,Dest ports %d,%d\n",
+             (iphead[20]<<8)+iphead[21],
+             (iphead[22]<<8)+iphead[23]);
+      printf("Layer-4 protocol %d\n",iphead[9]);
+    }
+  }
+  
+}
